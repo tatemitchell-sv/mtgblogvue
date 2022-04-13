d50f27dab996f71cbe70502220719f99ab30fa23
Tate Mitchell <htatemitchell@gmail.com>
2022-04-13 12:48:37 -0600

    Issue #3 parsed JSON for relevant card details

--- src/components/CardDetails.vue
+++ src/components/CardDetails.vue
@@ -6,8 +6,43 @@
     <h2>{{card.name}}</h2>
     <hr/>
     <img :src="card.imageUrl" />
+    <p>{{card.name}}</p><hr>
+    <p>{{card.manaCost}}</p><hr>
+    <p>{{card.type}}</p><hr>
+    <p>{{card.text}}</p><hr>
 
-    <pre>{{JSON.stringify(card, null, 4)}}</pre>
+    <ul>
+        <li v-for="ruling in card.rulings" :key="ruling">
+            {{ruling.date}} - {{ruling.text}}
+        </li>
+    </ul><hr>
+
+       <ul>
+        <li v-for="legality in card.legalities" :key="legality">
+            {{legality.format}} - {{legality.legality}}
+        </li>
+    </ul><hr>
+
+
+
+    <!-- <pre>{{JSON.stringify(card, null, 4)}}</pre> -->
+
+    <!-- 
+        Card details/properties to display:
+        -imageUrl as <img>
+        -name
+        -manaCost
+        -type
+        -text
+        -rulings
+        -legalities
+     -->
+
+     <footer></footer>
 </template>
 
-<style scoped></style>
+<style scoped>
+    footer {
+        height: 50px;
+    }
+</style>
