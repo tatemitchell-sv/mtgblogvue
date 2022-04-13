7735f5b4331e74230c00dd6b74564e26852b994b
Tate Mitchell <htatemitchell@gmail.com>
2022-04-13 16:44:45 -0600

    Issue #7 Replaced bracket references with corresponding symbols in card text

--- src/components/CardDetails.vue
+++ src/components/CardDetails.vue
@@ -3,88 +3,89 @@ defineProps({ card: Object })
 </script>
 
 <template>
-    <h2>{{ card.name }}</h2>
-    <hr />
+    <div class="cardDetails">
+        <h2>{{ card.name }}</h2>
+        <hr />
 
-    <div class="row">
-        <div class="cardFrame"><img :src="card.imageUrl" /></div>
-        <div class="cardText">
-            <p>{{ card.name }}</p>
-            <p>{{ card.manaCost }}</p>
-            <p>{{ card.type }}</p>
-            <p>{{ card.text }}</p>
+        <div class="row">
+            <div class="cardFrame"><img :src="card.imageUrl" /></div>
+            <div class="cardText">
+                <p>{{ card.name }}</p>
+                <p v-html="'Mana Cost: ' + card.manaCost"></p>
+                <p>{{ card.type }}</p>
+                <p v-html="card.text"></p>
+            </div>
         </div>
+        <hr />
+        <h3>Card Rulings</h3>
+        <ul>
+            <li v-for="ruling in card.rulings" :key="ruling">
+                {{ ruling.date }} - {{ ruling.text }}
+            </li>
+        </ul>
+        <hr>
+
+        <h3>Card Legalities</h3>
+        <ul>
+            <li v-for="legality in card.legalities" :key="legality">
+                {{ legality.format }} - {{ legality.legality }}
+            </li>
+        </ul>
+        <hr>
+
+        <!-- <pre>{{JSON.stringify(card, null, 4)}}</pre> -->
+
+        <footer></footer>
     </div>
-    <hr />
-    <h3>Card Rulings</h3>
-    <ul>
-        <li v-for="ruling in card.rulings" :key="ruling">
-            {{ ruling.date }} - {{ ruling.text }}
-        </li>
-    </ul>
-    <hr>
-
-    <h3>Card Legalities</h3>
-    <ul>
-        <li v-for="legality in card.legalities" :key="legality">
-            {{ legality.format }} - {{ legality.legality }}
-        </li>
-    </ul>
-    <hr>
-
-    <!-- <pre>{{JSON.stringify(card, null, 4)}}</pre> -->
-
-    <!-- 
-        Card details/properties to display:
-        -imageUrl as <img>
-        -name
-        -manaCost
-        -type
-        -text
-        -rulings
-        -legalities
-     -->
-
-    <footer></footer>
 </template>
 
-<style scoped>
-footer {
+<style>
+.cardDetails footer {
     height: 50px;
 }
 
-.row {
-    display:flex;
+.cardDetails .row {
+    display: flex;
     padding: 20px 0px;
 }
 
-img {
+.cardDetails img {
     width: 100%;
     height: auto;
     border-radius: 4%;
-    box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
+    box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
 }
 
-.cardFrame {
+.cardDetails .cardFrame {
     width: 35%;
 }
-.cardText {
+
+.cardDetails .cardText {
     width: calc(55% - 30px);
     padding: 0px 30px;
 }
-p {
+
+.cardDetails p {
     padding-left: 20px;
     padding-top: 20px;
 }
 
-ul {
+.cardDetails .manasymbol {
+    border-radius: 50%;
+    width: 20px;
+    height: auto;
+    margin: 0 2px;
+}
+
+.cardDetails ul {
     padding-bottom: 20px;
 }
-li {
+
+.cardDetails li {
     padding: 10px 10px;
 }
 
-h3 {
+.cardDetails h3 {
     padding: 20px 10px;
 }
 </style>
--- src/views/CardView.vue
+++ src/views/CardView.vue
@@ -1,10 +1,47 @@
 <script setup>
-    import { useRoute } from 'vue-router';
-    import CardDetails from "../components/CardDetails.vue"
-    import data from "../../apicarddata.js"
-    const route = useRoute();
-    const card = data.find(card=>card.id===route.params.id);
-    console.log(card)
+import { useRoute } from 'vue-router';
+import CardDetails from "../components/CardDetails.vue"
+import data from "../../apicarddata.js"
+const route = useRoute();
+
+const replaceWithImg = str => {
+    str = str.replaceAll("{W}", "<img class='manasymbol' src='@/../../src/assets/manaW.png' />")
+    str = str.replaceAll("{U}", "<img class='manasymbol' src='@/../../src/assets/manaU.png' />")
+    str = str.replaceAll("{B}", "<img class='manasymbol' src='@/../../src/assets/manaB.png' />")
+    str = str.replaceAll("{R}", "<img class='manasymbol' src='@/../../src/assets/manaR.png' />")
+    str = str.replaceAll("{G}", "<img class='manasymbol' src='@/../../src/assets/manaG.png' />")
+    str = str.replaceAll("{W/U}", "<img class='manasymbol' src='@/../../src/assets/manaWU.png' />")
+    str = str.replaceAll("{W/B}", "<img class='manasymbol' src='@/../../src/assets/manaWB.png' />")
+    str = str.replaceAll("{U/B}", "<img class='manasymbol' src='@/../../src/assets/manaUB.png' />")
+    str = str.replaceAll("{U/R}", "<img class='manasymbol' src='@/../../src/assets/manaUR.png' />")
+    str = str.replaceAll("{B/R}", "<img class='manasymbol' src='@/../../src/assets/manaBR.png' />")
+    str = str.replaceAll("{B/G}", "<img class='manasymbol' src='@/../../src/assets/manaBG.png' />")
+    str = str.replaceAll("{R/W}", "<img class='manasymbol' src='@/../../src/assets/manaRW.png' />")
+    str = str.replaceAll("{R/G}", "<img class='manasymbol' src='@/../../src/assets/manaRG.png' />")
+    str = str.replaceAll("{G/W}", "<img class='manasymbol' src='@/../../src/assets/manaGW.png' />")
+    str = str.replaceAll("{G/U}", "<img class='manasymbol' src='@/../../src/assets/manaGU.png' />")
+    str = str.replaceAll("{X}", "<img class='manasymbol' src='@/../../src/assets/manaX.png' />")
+    str = str.replaceAll("{Y}", "<img class='manasymbol' src='@/../../src/assets/manaY.png' />")
+    str = str.replaceAll("{Z}", "<img class='manasymbol' src='@/../../src/assets/manaZ.png' />")
+    str = str.replaceAll("{T}", "<img class='manasymbol' src='@/../../src/assets/tapArrow.png' />")
+
+    for (let i=0; i < 21; i++) {
+        str = str.replaceAll("{" + i + "}", "<img class='manasymbol' src='@/../../src/assets/mana" + i + ".png' />")
+    }
+
+    return str;
+}
+
+const formattedData = data.map(card => {
+    return {
+        ...card,
+        manaCost: replaceWithImg(card.manaCost),
+        text: replaceWithImg(card.text)
+    }
+})
+const card = formattedData.find(card => card.id === route.params.id);
+
+console.log(card)
 </script>
 
 <template>
