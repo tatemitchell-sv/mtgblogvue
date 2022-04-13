55fb8b989753db670b6f74be36bbf86b5c5a24d9
Tate Mitchell <htatemitchell@gmail.com>
2022-04-13 12:59:13 -0600

    Issue #4 HTML updated to display content with proper headers and labels

--- src/components/CardDetails.vue
+++ src/components/CardDetails.vue
@@ -1,29 +1,40 @@
 <script setup>
-    defineProps({card: Object})
+defineProps({ card: Object })
 </script>
 
 <template>
-    <h2>{{card.name}}</h2>
-    <hr/>
-    <img :src="card.imageUrl" />
-    <p>{{card.name}}</p><hr>
-    <p>{{card.manaCost}}</p><hr>
-    <p>{{card.type}}</p><hr>
-    <p>{{card.text}}</p><hr>
+    <h2>{{ card.name }}</h2>
+    <hr />
 
+    <div class="row">
+        <div><img :src="card.imageUrl" /></div>
+        <div>
+            <p>{{ card.name }}</p>
+            <hr>
+            <p>{{ card.manaCost }}</p>
+            <hr>
+            <p>{{ card.type }}</p>
+            <hr>
+            <p>{{ card.text }}</p>
+            <hr>
+        </div>
+    </div>
+
+    <h3>Card Rulings</h3>
     <ul>
         <li v-for="ruling in card.rulings" :key="ruling">
-            {{ruling.date}} - {{ruling.text}}
+            {{ ruling.date }} - {{ ruling.text }}
         </li>
-    </ul><hr>
+    </ul>
+    <hr>
 
-       <ul>
+    <h3>Card Legalities</h3>
+    <ul>
         <li v-for="legality in card.legalities" :key="legality">
-            {{legality.format}} - {{legality.legality}}
+            {{ legality.format }} - {{ legality.legality }}
         </li>
-    </ul><hr>
-
-
+    </ul>
+    <hr>
 
     <!-- <pre>{{JSON.stringify(card, null, 4)}}</pre> -->
 
@@ -38,11 +49,11 @@
         -legalities
      -->
 
-     <footer></footer>
+    <footer></footer>
 </template>
 
 <style scoped>
-    footer {
-        height: 50px;
-    }
+footer {
+    height: 50px;
+}
 </style>
